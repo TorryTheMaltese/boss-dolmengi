2b1092460c67359284795aac26d276963fdf1f61
TorryTheMaltese <torryiscute@gmail.com>
2023-11-09 22:16:29 +0900

    commit test

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/dolmengi-boss-party",
+  publicPath: "/boss-dolmengi",
   outputDir: path.resolve(__dirname, "docs"),
 });
