54a1a8ea5d8c3af00f4923efe73fcf95ed4467dc
TorryTheMaltese <torryiscute@gmail.com>
2023-11-09 21:05:20 +0900

    re config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/dolmengi",
+  publicPath: "/dolmengi-boss-party",
   outputDir: "docs",
 });
