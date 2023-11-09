6e17d9c70a05ca61ef08b5b711e19049f814ce1c
TorryTheMaltese <torryiscute@gmail.com>
2023-11-09 21:40:41 +0900

    edit output dir path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/dolmengi-boss-party",
-  outputDir: "docs",
+  outputDir: path.resolve(__dirname, "./docs"),
 });
